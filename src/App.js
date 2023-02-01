ad346a059672e448e18c5e59743e243a69e18cb0
adityadevcode <87442837+adityadevcode@users.noreply.github.com>
2023-02-01 17:21:27 -0500

    front end form component sending to nodejs server

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import LoginForm from "./Components/LoginForm";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <p>Form submission backend</p>
+     <LoginForm />
     </div>
   );
 }
 
 export default App;
+
+
+// backend c0de
+//  -This code sets up an Express.js server listening on port 3000 and listens
+//  for a POST request to the /login endpoint. 
+//  -When a request is received, it parses the request body to extract
+//   the username and password fields, and returns a JSON response with
+//    a success field set to true.
+
+// -You can now use your frontend code to send a POST request to the
+//  /login endpoint with a username and password in the request body.
+//   The response from the server will be the { success: true } JSON object.
+
+
+// const express = require('express');
+// const app = express();
+// const cors =require('cors');
+// const port=3000;
+
+// app.use(express.json());
+
+// app.use(cors({
+//       origin:'*'
+//   }))
+
+// app.post('/login', (req, res) => {
+//     const {username, password} = req.body;
+//      // Check if the username and password match
+//   // ...
+//   // Store the details in a database or memory
+//   // ...
+//   res.json({ success: true });
+// })
+
+// app.listen(port, ()=> {
+//     console.log(`Server running at http://localhost:${port}`);
+// });
---
+++ src/Components/LoginForm.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+
+const LoginForm = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetch('http://localhost:3000/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ username, password }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Username"
+        value={username}
+        onChange={(event) => setUsername(event.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(event) => setPassword(event.target.value)}
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default LoginForm;
